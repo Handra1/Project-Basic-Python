a356d53dc2d49261423a85d22c112b17b8055be3
Han <101443278+Handra1@users.noreply.github.com>
2022-03-14 12:25:38 +0700

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,17 @@
+print("Handra Andreas")
+print("Hello World")
+"""value terakhir yg diambil"""
+likes = 25
+likes = 22
+likes = 35
+print(likes)
+
+first_name = "Handra"
+last_name = "Andreas"
+full = first_name + last_name
+print(full)
+
+first_name = "Handra"
+last_name = "Andreas"
+full = first_name + " " + last_name
+print(full)
